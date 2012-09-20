a3e5d5ee4d8a22b9df3fc8d5806c123cab36eb78
David Dollar <ddollar@gmail.com>
2012-09-20 13:46:20 -0400

    update for hpg client v11, fixes #3

--- init.rb
+++ init.rb
@@ -75,7 +75,7 @@ private
     if (uri = URI.parse(db_name)).scheme
       "#{uri.path[1..-1]} on #{uri.host||"localhost"}:#{uri.port||5432}"
     else
-      "#{hpg_resolve(db_name).first} on #{app}.herokuapp.com"
+      "#{hpg_resolve(db_name).config_var} on #{app}.herokuapp.com"
     end
   end
 
@@ -83,7 +83,7 @@ private
     if URI.parse(name_or_url).scheme
       name_or_url
     else
-      hpg_resolve(name_or_url).last
+      hpg_resolve(name_or_url).url
     end
   end
 
